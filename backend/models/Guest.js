fec01c2caeb36fabb7122449c73d6374ea472f2e
HerambInamke <heramb15012006@gmail.com>
2025-03-21 16:23:56 +0530

    proper name of the guest.js/models

--- backend/models/Guest.js
+++ backend/models/Guest.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+
 const guestSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   name: { type: String, required: true },
