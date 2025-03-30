a5191b988733ccda13de49248c3e0e09ed20a38a
HerambInamke <heramb15012006@gmail.com>
2025-03-30 18:11:12 +0530

    added the database schema

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,37 +1,38 @@
-// import mongoose from 'mongoose';
-// const Schema = mongoose.Schema;
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
 
-// const taskSchema = new Schema({
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     description: {
-//         type: String
-//     },
-//     dueDate: {
-//         type: Date,
-//         required: true
-//     },
-//     assignedTo: {
-//         type: Schema.Types.ObjectId,
-//         ref: 'User',
-//         required: true
-//     },
-//     wedding: {
-//         type: Schema.Types.ObjectId,
-//         ref: 'Wedding',
-//         required: true
-//     },
-//     status: {
-//         type: String,
-//         enum: ['Pending', 'In Progress', 'Completed'],
-//         default: 'Pending'
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: Date.now
-//     }
-// });
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    dueDate: {
+        type: Date,
+        required: true
+    },
+    assignedTo: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    wedding: {
+        type: Schema.Types.ObjectId,
+        ref: 'Wedding',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'In Progress', 'Completed'],
+        default: 'Pending'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
 
-// export default mongoose.model('Task', taskSchema);
+export default mongoose.model('Task', taskSchema);
