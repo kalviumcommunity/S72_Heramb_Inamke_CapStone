a5f0ec9dcc322028c82c8589370fe7f10ffdd3e2
HerambInamke <heramb15012006@gmail.com>
2025-03-30 18:01:26 +0530

    have to add the schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,34 +1,34 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-import bcrypt from 'bcryptjs';
+// import mongoose from 'mongoose';
+// const Schema = mongoose.Schema;
+// import bcrypt from 'bcryptjs';
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['couple', 'vendor'], required: true },
-  wedding: {
-    type: Schema.Types.ObjectId,
-    ref: 'Wedding'
-  },
-  tasks: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Task'
-  }],
-  createdAt: { type: Date, default: Date.now },
-  weddings: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Wedding'
-  }],
-  participatingWeddings: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Wedding',
-    role: {
-      type: String,
-      enum: ['guest', 'bridesmaid', 'groomsman', 'other'],
-      default: 'guest'
-    }
-  }]
-});
+// const userSchema = new Schema({
+//   name: { type: String, required: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   role: { type: String, enum: ['couple', 'vendor'], required: true },
+//   wedding: {
+//     type: Schema.Types.ObjectId,
+//     ref: 'Wedding'
+//   },
+//   tasks: [{
+//     type: Schema.Types.ObjectId,
+//     ref: 'Task'
+//   }],
+//   createdAt: { type: Date, default: Date.now },
+//   weddings: [{
+//     type: Schema.Types.ObjectId,
+//     ref: 'Wedding'
+//   }],
+//   participatingWeddings: [{
+//     type: Schema.Types.ObjectId,
+//     ref: 'Wedding',
+//     role: {
+//       type: String,
+//       enum: ['guest', 'bridesmaid', 'groomsman', 'other'],
+//       default: 'guest'
+//     }
+//   }]
+// });
 
-export default mongoose.model('User', userSchema);
+// export default mongoose.model('User', userSchema);
