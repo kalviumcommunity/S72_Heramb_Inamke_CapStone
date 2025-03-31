9870118df1ffbe98ea9845f8d9172d8c2c96b780
HerambInamke <heramb15012006@gmail.com>
2025-03-31 17:56:00 +0530

    .

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  
   role: { type: String, enum: ['couple', 'vendor'], required: true },
   wedding: {
     type: Schema.Types.ObjectId,
